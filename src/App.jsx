8a5b756d265da1fcb2e078cce7aab5f760c48916
myeongju <kmj042894@gmail.com>
2025-03-30 20:52:25 +0900

    20250330 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
+import Header from './components/Header';
 
 function App() {
 
   return (
-    <div>
-      <h1>My Todolist</h1>
+    <div className='App'>
+      <Header />
+      <div>Todo Editor</div>
+      <div>TodoList</div>
     </div>
-  )
-}
+  );
+};
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return (
+        <div className="Header">Header Component</div>
+    );
+};
+
+export default Header;
