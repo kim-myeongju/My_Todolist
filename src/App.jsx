3c85a5ef62e633f8c7564d276d33865a5a6e85f6
myeongju <kmj042894@gmail.com>
2025-03-31 22:41:21 +0900

    20250331 useState add

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import './App.css'
 import Header from './components/Header';
 import TodoEditor from './components/TodoEditor';
 import TodoList from './components/TodoList';
+import { useState } from 'react';
 
 function App() {
+  const [todo, setTodo] = useState([]);
 
   return (
     <div className='App'>
